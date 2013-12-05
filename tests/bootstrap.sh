e0f187c21d950c4f19223ab5dd0fe2c877862409
Gasol Wu <gasol.wu@gmail.com>
2013-12-06 03:16:16 +0800

    Add Vagrantfile

---
+++ tests/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo debconf-set-selections <<< 'nis nis/domian string precise32'
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y expect php5-cli php5-dev nis valgrind
+sudo sed -i'' -re 's/NISSERVER=(.*)/NISSERVER=true/' /etc/default/nis
+sudo service ypserv start
+sudo expect -c '
+spawn /usr/lib/yp/ypinit -m
+expect "<control D>"
+send ""
+expect "Is this correct?"
+send "\n"
+expect eof
+'
