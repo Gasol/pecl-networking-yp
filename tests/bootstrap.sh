af79c508d6d9b6e6b50b0b0ee33f9abf6db320d5
Gasol Wu <gasolwu@php.net>
2014-02-24 18:48:36 +0800

    Fix typo

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo debconf-set-selections <<< 'nis nis/domian string precise32'
+sudo debconf-set-selections <<< 'nis nis/domain string precise32'
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y expect php5-cli php5-dev nis valgrind
 sudo sed -i'' -re 's/NISSERVER=(.*)/NISSERVER=true/' /etc/default/nis
 sudo service ypserv start
