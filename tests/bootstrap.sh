f9eae5e7e7e4e19c49285f6c576f391c2d7ca5b3
Gasol Wu <gasolwu@php.net>
2014-02-25 00:32:15 +0800

    Don't supply extension for sed

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -9,9 +9,9 @@ fi
 sudo apt-get update
 sudo debconf-set-selections <<< 'nis nis/domain string precise32'
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y expect php5-cli php5-dev nis valgrind
-sudo sed -i'' -re 's/NISSERVER=(.*)/NISSERVER=true/' /etc/default/nis
-sudo sed -i'' -re "s|(YPSRCDIR = ).*|\\1$YPSRCDIR|" /var/yp/Makefile
-sudo sed -i'' -re "s|(YPPWDDIR = ).*|\\1$YPSRCDIR|" /var/yp/Makefile
+sudo sed -i -re 's/NISSERVER=(.*)/NISSERVER=true/' /etc/default/nis
+sudo sed -i -re "s|(YPSRCDIR = ).*|\\1$YPSRCDIR|" /var/yp/Makefile
+sudo sed -i -re "s|(YPPWDDIR = ).*|\\1$YPSRCDIR|" /var/yp/Makefile
 sudo service ypserv start
 sudo expect -c '
 spawn /usr/lib/yp/ypinit -m
