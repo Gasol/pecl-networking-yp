3a90eaf79910bf44d164a90841d46be8f6156734
Gasol Wu <gasolwu@php.net>
2014-02-24 22:35:53 +0800

    Support both Vagrant and Travis

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-YPSRCDIR="$TRAVIS_BUILD_DIR/tests/data"
+if [ -z "$TRAVIS_BUILD_DIR" ]; then
+	YPSRCDIR="/vagrant/tests/data"
+else
+	YPSRCDIR="$TRAVIS_BUILD_DIR/tests/data"
+fi
 
 sudo apt-get update
 sudo debconf-set-selections <<< 'nis nis/domain string precise32'
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y expect nis valgrind
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y expect php5-cli php5-dev nis valgrind
 sudo sed -i'' -re 's/NISSERVER=(.*)/NISSERVER=true/' /etc/default/nis
 sudo sed -i'' -re "s|(YPSRCDIR = ).*|\\1$YPSRCDIR|" /var/yp/Makefile
 sudo sed -i'' -re "s|(YPPWDDIR = ).*|\\1$YPSRCDIR|" /var/yp/Makefile
