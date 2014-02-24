9ee18bd889ff0ea71c6e6aa555dcd4e21f9902d5
Gasol Wu <gasolwu@php.net>
2014-02-24 14:19:38 +0800

    Integrate with Travis CI

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo debconf-set-selections <<< 'nis nis/domain string precise32'
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y expect php5-cli php5-dev nis valgrind
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y expect nis valgrind
 sudo sed -i'' -re 's/NISSERVER=(.*)/NISSERVER=true/' /etc/default/nis
 sudo sed -i'' -re 's|(YPSRCDIR = ).*|\1/vagrant/tests/data|' /var/yp/Makefile
 sudo sed -i'' -re 's|(YPPWDDIR = ).*|\1/vagrant/tests/data|' /var/yp/Makefile
